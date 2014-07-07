5b9f6377b4f7fe2b68d18f44d79b72938354e7ec
Kevin Lu <kevyclvt@gmail.com>
2014-07-07 14:09:00 -0400

    code upload

---
+++ App_Code/LocalEnv.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for LocalEnv
+/// </summary>
+public static class LocalEnv
+{
+    //static public string AVLDBConnString = "Server=(local);Database=AVL;User Id=tirs;Password=keepsafe;";
+    static public string tirsConnString = "Server=(local);Database=tirs;User Id=tirs;Password=keepsafe;";
+
+    static public string AVLDBConnString = "Server=10.40.16.106;Database=DCVehiclelog2011;User Id=sa;Password=cadds111!;";
+    //static public string tirsConnString = "Server=10.40.62.167;Database=tirs;User Id=tirs;Password=keepsafe;";
+    static public string speedConnString = "Server=10.40.62.167;Database=speed;User Id=tirs;Password=keepsafe;";
+    static public string microwaveConnString = "Server=10.40.63.52;Database=RTMSData;User Id=tirs;Password=keepsafe;";
+    static public string tirs_lookupConnString = "Server=10.40.62.167;Database=tirs_lookup;User Id=tirs;Password=keepsafe;";
+    
+}
